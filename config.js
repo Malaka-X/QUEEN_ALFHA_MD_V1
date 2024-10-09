e4f919357c9180e895c0c38884969ac12682e693
Malaka-X <malakamdbot@gmail.com>
2024-10-09 19:22:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -9,4 +9,5 @@ SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/106251140?v=4",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am YourName i am alive now!",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATU || "true",
+MODE: process.env.MODE || "public",
 };
