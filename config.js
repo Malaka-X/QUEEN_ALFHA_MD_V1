56640b5ed16fb59a6f4096d6227575d0659f0e0f
Malaka-X <malakamdbot@gmail.com>
2024-10-09 10:46:09 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID,
 ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent.com/u/106251140?v=4",
 ALIVE_MSG: process.env.ALIVE_MSG || "Hello, I am YourName i am alive now!",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATU || "true",
 };
