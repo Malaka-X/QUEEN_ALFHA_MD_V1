f86bf2db8eca711088aa339257ced537838da6a1
Malaka-X <malakamdbot@gmail.com>
2024-10-09 19:08:20 +0530

    Update ai.js

--- plugins/ai.js
+++ plugins/ai.js
@@ -1,6 +1,6 @@
 const config = require('../config')
 const {cmd , commands} = require('../command')
-const { fetchJson } = require('../lib/functions')
+const {fetchJson} = require('../lib/functions')
 cmd({
     pattern: "ai",
     desc: "ai chat",
