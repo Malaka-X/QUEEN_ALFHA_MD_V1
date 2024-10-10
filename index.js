971e91ba41eaf79fb2c27aada5c9caa728ec7d79
Malaka-X <malakamdbot@gmail.com>
2024-10-11 00:31:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const util = require('util')
 const { sms,downloadMediaMessage } = require('./lib/msg')
 const axios = require('axios')
 const { File } = require('megajs')
-const prefix = '.'
+
 
 const ownerNumber = ['94704243771']
 
@@ -43,7 +43,11 @@ async function connectToWA() {
 const connectDB = require('./lib/mongodb')
 connectDB();
 //==============================================
-        
+const {readEnv} = require('./lib/database')   
+const config = await readEnv();
+const prefix = config.PREFIX
+//==============================================
+
 console.log("Connecting queenaifa bot 💗...");
 const { state, saveCreds } = await useMultiFileAuthState(__dirname + '/auth_info_baileys/')
 var { version } = await fetchLatestBaileysVersion()
